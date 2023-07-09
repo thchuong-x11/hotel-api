85bfcd4f1a4286d6a2e34bbd83be39b1d97eec08
Laurent Tran <tranhoangchuongapcs@gmail.com>
2023-07-09 23:34:52 +0200

    chore: add run examples in readme and add improve suggestion

--- src/api/hotels/services/get-hotels-service.ts
+++ src/api/hotels/services/get-hotels-service.ts
@@ -156,3 +156,35 @@ export class GetHotelsService {
     });
   }
 }
+
+/**
+ * Improvement notes:
+ *
+ * 1- Caching the data from suppliers
+ * One easy improvement is to store the data from suppliers to reduce the loading time at each request.
+ * Plus this will externalize the data normalization outside the endpoint.
+ * Depending on how frequent the suppliers change their data, and with which mechanism they can provide us
+ * to listen to the update (push, fetch endpoints with timestamp checkpoint, etag, etc.), we can update the internal
+ * storage accordingly by performing a periodically update batch run.
+ *
+ * 2- Filtering
+ * If we can have our own version of hotel data proposed by point 1, there is nothing much that we can do to improve the
+ * filtering other than properly configuring the index of the database, partitioning the database based on the endpoint usage
+ * If it is not the case, we need to consider delegating the filtering to the supplier endpoints whenever it is possible.
+ * Otherwise, we'll have to query all the available entries from supplier endpoint and still maintaining some sort of transient
+ * version of all the supplier data just to perform the filtering procedure.
+ * We may also need to consider support more way to filter: by Geo (country or city), by name, by amenities available, etc.
+ *
+ * 3- Pagination or streaming
+ * It is not possible to deliver all the available results in one simple request because the results could yield 90% or more
+ * of the available data of any supplier - this, in turn, may cause huge memory consumption and long waiting time; any of which
+ * can easily render negative user experience, not to mention the risks of having the request timeout or cancelled.
+ * The remedy is to deliver the results by batch, either using pagination or streaming - the choice depends certainly
+ * on whether we can have an internal database (then pagination is possible and easy to do) or we have to completely rely on
+ * suppliers' database (then pagination is not as easy to implement as streaming)
+ *
+ * 4- Merging and normalizing data
+ * For some data, in particular, booking-conditions and description we may need to really combine the data provided by
+ * multiple suppliers to have a rich and useful information for the end-user. We may consider using generative AI like
+ * Chat GPT or Bard AI to help with this job.
+ */
